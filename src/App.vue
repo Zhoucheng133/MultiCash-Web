26e1b0e97f08befdd3a5a723469f1b30dc5e6113
zhoucheng <zhoucheng133@outlook.com>
2026-05-16 02:47:23 +0800

    添加一些依赖

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <v-btn>测试</v-btn>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import { createPinia } from 'pinia';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+createApp(App)
+.use(pinia)
+.use(vuetify)
+.mount('#app')
