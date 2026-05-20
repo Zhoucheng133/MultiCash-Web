21b32663e4d6595d2c49a314776480de4fb6329a
zhoucheng <zhoucheng133@outlook.com>
2026-05-20 17:50:26 +0800

    添加信息面板

---
+++ src/components/InfoPanel.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="panel shadow-xl">
+    <div class="title">{{ props.title }}</div>
+    <div class="content">{{ props.content }}</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(["title", "content"]);
+</script>
+
+<style scoped>
+@import "../styles/components/infopanel.css";
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,62 @@
 <template>
-  <TitleBar :is-login="true" />
-  <div class="main"></div>
+  <TitleBar :is-login="true" style="position: sticky;" />
+  <div class="main">
+    <div class="page">
+      <div class="section_title">
+        <div class="flex flex-col">
+          <div class="manage_title">银行卡管理</div>
+          <div class="all_title">全部银行卡</div>
+        </div>
+        <div class="section_title_actions">
+          <v-btn color="blue" rounded="lg" prepend-icon="mdi mdi-plus">
+            添加银行卡
+          </v-btn>
+        </div>
+      </div>
+      <div class="section_info_desktop">
+        <InfoPanel title="银行卡数量" content="3" />
+        <InfoPanel title="余额" content="¥123456.00" />
+        <InfoPanel title="启用中" content="3" />
+      </div>
+      <v-carousel height="120"
+        hide-delimiter-background 
+        :show-arrows="false" 
+        class="section_info_mobile"
+        hide-delimiters
+        v-model="cardIndex"
+      >
+        <v-carousel-item>
+          <div class="ml-1 mr-1">
+            <InfoPanel title="银行卡数量" content="3" />
+          </div>
+        </v-carousel-item>
+        <v-carousel-item>
+          <div class="ml-1 mr-1">
+            <InfoPanel title="余额" content="¥123456.00" />
+          </div>
+        </v-carousel-item>
+        <v-carousel-item>
+          <div class="ml-1 mr-1">
+            <InfoPanel title="启用中" content="3" />
+          </div>
+        </v-carousel-item>
+      </v-carousel>
+      <div class="cardIndexIndicator">
+        <i :class="cardIndex==0 ? 'fa-solid fa-circle' : 'fa-regular fa-circle'" style="font-size: 8px;"></i>
+        <i :class="cardIndex==1 ? 'fa-solid fa-circle' : 'fa-regular fa-circle'" style="font-size: 8px;"></i>
+        <i :class="cardIndex==2 ? 'fa-solid fa-circle' : 'fa-regular fa-circle'" style="font-size: 8px;"></i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import TitleBar from '../components/TitleBar.vue';
+import InfoPanel from '../components/InfoPanel.vue';
+import { ref } from 'vue';
 
-document.title = "MultiCash | 主页";
+const cardIndex = ref(0);
+
 </script>
 
 <style scoped>
